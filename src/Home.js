03a315fd896e409b660352cee8913d348c7dfe8d
James C <you@example.com>
2022-06-29 20:26:16 -0700

    feat: use home component with classnames and inline styling

---
+++ src/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className="HeaderText Italic" >Send them a smile with HappyCards!!</h1>
+            <h2 style={{ fontStyle: "italic", marginTop: 40}}>Choose from thousands of things...</h2>
+        </div>
+    );
+}
