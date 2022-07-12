96826c9f286335835af75b11f33114f580466b1d
James C <you@example.com>
2022-07-12 00:21:42 -0700

    feat: add omdb search functionality using function rather than useEffect

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function CardForm() {
-    const [recipientName, setRecipientName] = useState("");
-    const [recipientEmail, setRecipientEmail] = useState("");
-    const [selectedHolidayName, setSelectedHolidayName] = useState("");
-    const [senderName, setSenderName] = useState("");
-    const [senderEmail, setSenderEmail] = useState("");
+    // const [recipientName, setRecipientName] = useState("");
+    // const [recipientEmail, setRecipientEmail] = useState("");
+    // const [selectedHolidayName, setSelectedHolidayName] = useState("");
+    // const [senderName, setSenderName] = useState("");
+    // const [senderEmail, setSenderEmail] = useState("");
+
+    const [movieName, setMovieName] = useState("");
+    const [movieList, setMovielist] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     function handleSubmit() {
         console.log("Submitted!");
@@ -14,19 +18,53 @@ export default function CardForm() {
         return value.toUpperCase();
     }
 
+    const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        async function getMoviesByName() {
+            setIsLoading(true);
+            const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${movieName}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log("data is: ", data.Search);
+            setMovielist(data.Search);
+            setIsLoading(false);
+        }
+        getMoviesByName();
+    };
+
     return (
-        <div>
+        <div><br></br>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="recipientName">Their Name </label>
+                <label htmlFor="recipientName">Search by Movie Name </label>
                 <input 
                     name="recipientName"
                     type="text"
-                    value={makeUppercase(recipientName)}
-                    onChange={(event) => setRecipientName(event.target.value)}
+                    value={movieName}
+                    onChange={(event) => setMovieName(event.target.value)}
                 />
+                <button type="submit">Search</button>
+            
             </form>
             <br></br>
-            <span>{recipientName}</span>
+            <span>Results:</span>
+        <div>   
+            {!isLoading ? (
+                <div><br></br>
+                {movieList.length > 0 ? (
+                    movieList.map((Movie) => (
+                        <div>{Movie.Title} <span style={{ color: "lightblue" }}>{Movie.Year}</span></div>
+                    ))
+                ) : (
+                <div>No results found.</div>
+                )
+                }
+                </div>
+            ) : (
+            <div style={{ color: "blue" }} >Searching for your movies...</div>
+            )}
+        </div>
         </div>
     );
 }
