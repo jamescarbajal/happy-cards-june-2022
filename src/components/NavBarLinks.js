031e5469ff89926c4985a4f3e8528e6fcf85e063
James C <you@example.com>
2022-07-27 21:06:13 -0700

    feat: add conditional login/logout button

--- src/components/NavBarLinks.js
+++ src/components/NavBarLinks.js
@@ -1,25 +1,37 @@
 import { NavLink } from "react-router-dom";
 import { HeaderLink } from "./StyledComponents";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export default function NavBarLinks(props) {
+
+    const { loggedInUser, setLoggedInUser } = useContext(UserContext);
 
     function styleByActiveStatus(isActive) {
         return isActive 
         ? {fontWeight: "bold", textDecoration: "none"} 
         : {fontWeight: "normal", textDecoration: "none"};
     }
+    
 
     return (
     <>
-        <NavLink to="/" style={({ isActive}) => styleByActiveStatus(isActive)}>
+        <NavLink to="/" style={({ isActive }) => styleByActiveStatus(isActive)}>
             <HeaderLink path="/">Home</HeaderLink>
-        </NavLink>
-        <NavLink to="/Login" style={({ isActive}) => styleByActiveStatus(isActive)}>
-            <HeaderLink path="/Login">Login</HeaderLink>
         </NavLink>
         <NavLink to="/CardForm" style={({ isActive}) => styleByActiveStatus(isActive)}>
             <HeaderLink path="/CardForm">Card Form</HeaderLink>
         </NavLink>
+
+        {!loggedInUser ? (
+        <NavLink to="/Cardform" style={({ isActive }) => styleByActiveStatus(isActive)}>
+            <HeaderLink path="/Cardform">Login</HeaderLink>
+        </NavLink>
+        ) : (
+            <NavLink to="/Login" style={({ isActive }) => styleByActiveStatus(isActive)}>
+                <HeaderLink onClick={() => setLoggedInUser(null)} path="/Login">Logout</HeaderLink>
+            </NavLink>
+        )}
     </>
     );
 }
